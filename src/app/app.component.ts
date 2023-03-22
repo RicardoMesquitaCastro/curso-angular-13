ecae65498043b19165d6f685c8757f3902edc1b4
Ricardo <ricardomesquita12@hotmail.com>
2023-03-22 13:36:50 -0300

    #07-@input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  userName='Ricardo';
+
+  userData ={
+    email:'joaquim@email.com',
+    role:'Admin'
+  }
+
   title = 'curso-angular13';
 }
